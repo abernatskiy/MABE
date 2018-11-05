a09485bd83ab1639aee72dde38b10e6b2d061a27
Anton Bernatskiy (kohama) <abernats@uvm.edu>
2018-11-04 23:39:47 -0700

    Dataset handling utility class added
    	new file:   World/AsteroidGazingWorld/Utilities/AsteroidsDatasetParser.cpp
    	new file:   World/AsteroidGazingWorld/Utilities/AsteroidsDatasetParser.h

---
+++ World/AsteroidGazingWorld/Utilities/AsteroidsDatasetParser.cpp
@@ -0,0 +1,134 @@
+#include "AsteroidsDatasetParser.h"
+
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <cstdlib>
+#include <regex>
+#include <vector>
+
+// Auxiliary function definition, see https://stackoverflow.com/questions/874134
+
+bool hasEnding(std::string const &fullString, std::string const &ending) {
+
+	if (fullString.length() >= ending.length())
+		return (0 == fullString.compare(fullString.length()-ending.length(), ending.length(), ending));
+	else
+		return false;
+}
+
+// AsteroidDatasetParser class definitions
+
+AsteroidsDatasetParser::AsteroidsDatasetParser(std::string datasetPath) :
+
+	fsDatasetPath(fs::system_complete(datasetPath)) {
+	if( !fs::exists(fsDatasetPath) ) { std::cerr << "Dataset path " << fsDatasetPath << " does not exist" << std::endl; exit(EXIT_FAILURE); }
+	if( !fs::is_directory(fsDatasetPath) ) { std::cerr << "Dataset path " << fsDatasetPath << " is not a directory" << std::endl; exit(EXIT_FAILURE); }
+}
+
+std::set<std::string> AsteroidsDatasetParser::getAsteroidsNames() {
+
+	std::set<std::string> asteroidNames;
+	fs::directory_iterator end_iter;
+	for( fs::directory_iterator dir_itr(fsDatasetPath); dir_itr != end_iter; ++dir_itr ) {
+		try {
+			if( fs::is_directory(dir_itr->status()) )
+				asteroidNames.insert(dir_itr->path().filename().string());
+    } catch (const std::exception & ex) {
+      std::cerr << "Exception " << ex.what() << " occurred while trying to list dataset folder " << fsDatasetPath << std::endl;
+      exit(EXIT_FAILURE);
+    }
+  }
+	return asteroidNames;
+}
+
+std::string AsteroidsDatasetParser::getICQPath(std::string asteroidName) {
+
+	std::string icqpath = ( fsDatasetPath / fs::path(asteroidName) / fs::path("icq.txt") ).string();
+	checkIfRegularFile(icqpath);
+	return icqpath;
+}
+
+std::set<std::string> AsteroidsDatasetParser::getAllPicturePaths(std::string asteroidName) {
+
+	std::set<std::string> pictureName;
+	fs::path asteroidDir(asteroidName);
+	fs::directory_iterator end_iter;
+	for( fs::directory_iterator dir_itr(fsDatasetPath/asteroidDir); dir_itr != end_iter; ++dir_itr ) {
+		try {
+			std::string filename = dir_itr->path().filename().string();
+			if( fs::is_regular_file(dir_itr->status()) && hasEnding(filename, ".png") )
+				pictureName.insert(dir_itr->path().string());
+    } catch (const std::exception & ex) {
+      std::cerr << "Exception " << ex.what() << " occurred while trying to list an asteroid subfolder " << (fsDatasetPath/asteroidDir) << std::endl;
+      exit(EXIT_FAILURE);
+    }
+  }
+	return pictureName;
+}
+
+std::string AsteroidsDatasetParser::getPicturePath(std::string asteroidName, unsigned condition, unsigned distance, unsigned phase) {
+
+	std::ostringstream buffer;
+	buffer << "condition" << condition << "_distance" << distance << "_phase" << std::setfill('0') << std::setw(4) << phase << ".png";
+	std::string outpath = ( fsDatasetPath / fs::path(asteroidName) / fs::path(buffer.str()) ).string();
+	checkIfRegularFile(outpath);
+	return outpath;
+}
+
+std::map<std::string,std::set<unsigned>> AsteroidsDatasetParser::getAllParameterValues(std::string asteroidName) {
+
+	// Listing all available pictures
+	std::set<std::string> picPaths = getAllPicturePaths(asteroidName);
+
+	// Picking one picture and determining parameter names from its filename
+	std::string examplePicPath = *picPaths.begin();
+	std::size_t filenameStarts = examplePicPath.find_last_of('/');
+	std::string filename = examplePicPath.substr(filenameStarts+1);
+
+	std::size_t current, previous = 0;
+	current = filename.find('_');
+	std::vector<std::string> paramNames;
+	std::regex nameFinder("^([[:alpha:]]+)[0-9]+.*");
+	std::smatch m;
+	bool stop = false;
+	while( !stop ) {
+		if( current == std::string::npos ) stop = true;
+
+		std::string paramRecord = filename.substr(previous, current-previous);
+		std::regex_match(paramRecord, m, nameFinder);
+		paramNames.push_back(m[1]);
+		previous = current + 1;
+		current = filename.find('_', previous);
+	}
+
+	// Scanning all the picture file names for parameter values, storing the results in the output array
+	std::map<std::string,std::set<unsigned>> parameterValues;
+	std::ostringstream buffer;
+	for(std::string pn : paramNames) {
+		parameterValues[pn] = std::set<unsigned>();
+		buffer << pn << "([0-9]+)_";
+	}
+	std::regex paramValsExtractor(buffer.str().substr(0, buffer.str().size()-1));
+	for(std::string picpath : picPaths) {
+		std::size_t filenameBegins = picpath.find_last_of('/');
+		std::size_t filenameEnds = picpath.find_last_of('.');
+		std::string filename = picpath.substr(filenameBegins+1, filenameEnds-filenameBegins-1);
+
+		std::regex_match(filename, m, paramValsExtractor);
+		for(unsigned i=0; i<paramNames.size(); i++)
+			parameterValues[paramNames[i]].insert(std::stoul(m[i+1]));
+	}
+
+	return parameterValues;
+
+}
+
+void AsteroidsDatasetParser::checkIfRegularFile(std::string path) {
+
+	fs::path boostPath(path);
+	if( !fs::is_regular_file(fs::path(path)) ) {
+		std::cerr << "Path " << path << " does not lead to a regular file and it should" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+}
---
+++ World/AsteroidGazingWorld/Utilities/AsteroidsDatasetParser.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "boost/filesystem/operations.hpp"
+#include "boost/filesystem/path.hpp"
+
+#include <string>
+#include <map>
+#include <set>
+
+namespace fs = boost::filesystem;
+
+class AsteroidsDatasetParser {
+
+public:
+	AsteroidsDatasetParser(std::string datasetPath);
+	std::set<std::string> getAsteroidsNames();
+	std::string getICQPath(std::string asteroidName);
+	std::set<std::string> getAllPicturePaths(std::string asteroidName);
+	std::string getPicturePath(std::string asteroidName, unsigned condition, unsigned distance, unsigned phase);
+	std::map<std::string,std::set<unsigned>> getAllParameterValues(std::string asteroidName);
+
+private:
+	fs::path fsDatasetPath;
+	void checkIfRegularFile(std::string path);
+};
