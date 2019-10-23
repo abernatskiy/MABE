18daadde71c6968c7c4a88d78971ead14717bdc7
Anton Bernatskiy (kohama) <abernats@uvm.edu>
2019-10-23 17:23:48 -0400

    Compileable sketch of the new asteroids world supporting DAG brain compositions
    	modified:   World/AsteroidTeamGazingWorld/AsteroidTeamGazingWorld.cpp
    	modified:   World/AsteroidTeamGazingWorld/AsteroidTeamGazingWorld.h

--- World/AsteroidTeamGazingWorld/AsteroidTeamGazingWorld.cpp
+++ World/AsteroidTeamGazingWorld/AsteroidTeamGazingWorld.cpp
@@ -8,6 +8,10 @@
 
 #include "../AsteroidGazingWorld/Schedules/AsteroidGazingSchedules.h"
 
+#include "../../Brain/DEMarkovBrain/DEMarkovBrain.h"
+
+#include "../../Utilities/nlohmann/json.hpp" // https://github.com/nlohmann/json/ v3.6.1
+
 std::shared_ptr<ParameterLink<int>> AsteroidTeamGazingWorld::brainUpdatesPerAsteroidPL =
   Parameters::register_parameter("WORLD_ASTEROID_TEAM_GAZING-brainUpdatesPerAsteroid", 100,
                                  "number of time steps for which the brain is allowed to gaze at each asteroid");
@@ -30,9 +34,6 @@ std::shared_ptr<ParameterLink<int>> AsteroidTeamGazingWorld::numRandomInitialCon
 	                               "Note: only relatve saccading sensors support this ATM. Non-positive values correspond to\n"
 	                               "one evaluation per view with default initial conditions (retina in the upper left corner for\n"
 	                               "relative saccading sensors). (default: -1)");
-std::shared_ptr<ParameterLink<int>> AsteroidTeamGazingWorld::compressToBitsPL =
-  Parameters::register_parameter("WORLD_ASTEROID_TEAM_GAZING-compressToBits", 20,
-                                 "if CompressedMentalImage is used, how many bits should it compress the input to? (default: 20)");
 std::shared_ptr<ParameterLink<int>> AsteroidTeamGazingWorld::fastRepellingPLInfoNumNeighborsPL =
   Parameters::register_parameter("WORLD_ASTEROID_TEAM_GAZING-fastRepellingPLInfoNumNeighbors", 10,
                                  "if CompressedMentalImage is used AND fastRepellingPatternLabelInformation is being computed,\n"
@@ -54,26 +55,30 @@ std::shared_ptr<ParameterLink<double>> AsteroidTeamGazingWorld::leakDecayRadiusP
                                  "if CompressedMentalImage is used AND any kind of repelling mutual information is being computed,\n"
 	                               "the leaks between pattern-label pairs will be computed as b*exp(-d/a), where d is the Hammming\n"
 	                               "distance from the neighbor of the point. What the value of a should be? Cannot be zero. (default: 1.)");
+std::shared_ptr<ParameterLink<std::string>> AsteroidTeamGazingWorld::pathToBrainTreeJSONPL =
+  Parameters::register_parameter("WORLD_ASTEROID_TEAM_GAZING-pathToBrainTreeJSON", (std::string) "./braindag.json",
+                                 "path to the JSON file describing how the multiple brains should be connected and where to read the constant ones");
 
 int AsteroidTeamGazingWorld::initialConditionsInitialized = 0;
 std::map<std::string,std::vector<Range2d>> AsteroidTeamGazingWorld::commonRelativeSensorsInitialConditions;
 
-AsteroidTeamGazingWorld::AsteroidTeamGazingWorld(std::shared_ptr<ParametersTable> PT_) : AbstractSlideshowWorld(PT_) {
+AsteroidTeamGazingWorld::AsteroidTeamGazingWorld(std::shared_ptr<ParametersTable> PT_) :
+	AbstractSlideshowWorld(PT_),
+	numBrains(0),
+	numBrainsOutputs(0) {
 
-	// Localizing settings
+	// Localizing and validating settings
 	brainUpdatesPerAsteroid = brainUpdatesPerAsteroidPL->get(PT_);
 	datasetPath = datasetPathPL->get(PT_);
 	std::string sensorType = sensorTypePL->get(PT_);
 	bool randomizeInitialConditions = (numRandomInitialConditionsPL->get(PT_) > 0);
 	unsigned numRandomInitialConditions = static_cast<unsigned>(numRandomInitialConditionsPL->get(PT_));
-
-	// Validating settings
 	if(randomizeInitialConditions && sensorType!="relative") {
 		std::cerr << "Sensors of type " << sensorType << " do not support initial conditions randomization" << std::endl;
 		exit(EXIT_FAILURE);
 	}
 
-	// Drawing the rest of the owl
+	// Preparing the sensors and their environment
 	datasetParser = std::make_shared<AsteroidsDatasetParser>(datasetPath);
 	currentAsteroidName = std::make_shared<std::string>("");
 
@@ -110,10 +115,116 @@ AsteroidTeamGazingWorld::AsteroidTeamGazingWorld(std::shared_ptr<ParametersTable
 	}
 
 	sensors->writeSensorStats();
+
+	// Preparing the brain graph
+	std::ifstream brainsJSONStream(pathToBrainTreeJSONPL->get(PT_));
+	nlohmann::json brainsJSON;
+	brainsJSON << brainsJSONStream;
+
+	brainsDiagram.insertNode(-1, {}); // the sensor
+
+	unsigned numEvolvableBrains = 0;
+	for(const auto& brainJSON : brainsJSON) { // for each brain description on the JSON
+		if(brainJSON["type"]!="DEMarkovBrain") {
+			std::cerr << "AsteroidTeamGazingWorld: components of types other than DEMArkovBrain are not surrently supported" << std::endl;
+			exit(EXIT_FAILURE);
+		}
+
+		// add a node to the map graph
+		std::vector<int> componentParentIdxs;
+		for(size_t ci=0; ci<numBrains; ci) { // component index
+			for(const auto& parentID : brainJSON["feedsFrom"]) {
+				if(parentID==brainsIDs[ci])
+					componentParentIdxs.push_back(ci);
+				else if(parentID=="thesensor")
+					componentParentIdxs.push_back(-1);
+			}
+		}
+		brainsDiagram.insertNode(numBrains, componentParentIdxs);
+
+		// make records where it is possible without conditioning on evolvability
+		brainsIDs.push_back(brainJSON["id"]);
+		brainsNumOutputs.push_back(brainJSON["numOutputs"]);
+		brainsNumHidden.push_back(brainJSON["numHidden"]);
+		brainsInputCached.push_back(brainJSON["cacheInputs"] ? 1 : 0);
+		brainsInputCache.push_back({});
+		if(brainJSON["exposeOutput"]) {
+			exposedOutputs.push_back(numBrains);
+			numBrainsOutputs += brainsNumOutputs.back();
+		}
+
+		// depending on whether the brain is evolvable or not, we do quite a few things differently
+		if(brainJSON["evolvable"]) {
+			brainsEvolvable.push_back(1);
+			numEvolvableBrains++;
+			if(numEvolvableBrains>1) {
+				std::cerr << "AsteroidTeamGazingWorld: DAG brains with more than one evolvable component are not supported at the moment" << std::endl;
+				exit(EXIT_FAILURE);
+			}
+			if(PT_->lookupInt("BRAIN_DEMARKOV-hiddenNodes") != brainsNumHidden.back()) {
+				std::cerr << "AsteroidTeamGazingWorld: number of hidden nodes in MABE config (" << (PT_->lookupInt("BRAIN_DEMARKOV-hiddenNodes"))
+				          << ") must be equal to the number of hidden nodes for the evolvable component listed in the brain DAG JSON description ("
+				          << brainsNumHidden.back() << ", read from " << pathToBrainTreeJSONPL->get(PT_) << ")" << std::endl;
+				exit(EXIT_FAILURE);
+			}
+
+			brains.push_back(nullptr);
+		}
+		else {
+			brainsEvolvable.push_back(0);
+
+			// constructing the constant component
+
+			shared_ptr<ParametersTable> curPT = make_shared<ParametersTable>("", nullptr);
+			// component-specific parameters
+			curPT->setParameter("BRAIN_DEMARKOV-hiddenNodes", static_cast<int>(brainsNumHidden.back()));
+			///// Parameter forwarding /////
+			curPT->setParameter("BRAIN_DEMARKOV_ADVANCED-recordIOMap",
+			                    PT_->lookupBool("BRAIN_DEMARKOV_ADVANCED-recordIOMap"));
+			curPT->setParameter("BRAIN_MARKOV_GATES_DETERMINISTIC-IO_Ranges",
+			                    PT_->lookupString("BRAIN_MARKOV_GATES_DETERMINISTIC-IO_Ranges"));
+			const vector<string> forwardedDoubleParamNames = { "BRAIN_DEMARKOV-gateInsertionProbability",
+			                                                   "BRAIN_DEMARKOV-gateDuplicationProbability",
+			                                                   "BRAIN_DEMARKOV-gateDeletionProbability",
+			                                                   "BRAIN_DEMARKOV-connectionToTableChangeRatio" };
+			for(const auto& param : forwardedDoubleParamNames)
+				curPT->setParameter(param, PT_->lookupDouble(param));
+			const vector<string> forwardedIntParamNames = { "BRAIN_DEMARKOV-initialGateCount",
+			                                                "BRAIN_DEMARKOV-minGateCount" };
+			for(const auto& param : forwardedIntParamNames)
+				curPT->setParameter(param, PT_->lookupInt(param));
+			///// Parameter forwarding END /////
+
+			unsigned numComponentInputs = 0;
+			for(int parentIdx : brainsDiagram.getParents(numBrains)) {
+				if(parentIdx<0)
+					numComponentInputs += sensors->numOutputs();
+				else
+					numComponentInputs += brainsNumOutputs[parentIdx];
+			}
+
+			std::shared_ptr<AbstractBrain> component = DEMarkovBrain_brainFactory(numComponentInputs, brainsNumOutputs.back(), curPT);
+
+			// deserializing the description file
+			std::ifstream componentFile(brainJSON["loadFrom"]);
+			std::string componentJSONStr;
+			std::getline(componentFile, componentJSONStr);
+
+			std::string name("currentComponent");
+			std::unordered_map<std::string,std::string> surrogateOrgData { { std::string("BRAIN_") + name + "_json", std::string("'") + componentJSONStr + std::string("'") } };
+      component->deserialize(curPT, surrogateOrgData, name);
+
+			brains.push_back(component);
+		}
+
+		numBrains++;
+	}
+
+	// Preparing the mental Image and adding motors that will be shaping it
 	mentalImage = std::make_shared<CompressedMentalImage>(currentAsteroidName,
 	                                                      datasetParser,
 	                                                      sensors,
-	                                                      compressToBitsPL->get(PT_),
+	                                                      numBrainsOutputs,
 	                                                      mihPatternChunkSizeBitsPL->get(PT_),
 	                                                      fastRepellingPLInfoNumNeighborsPL->get(PT_),
 	                                                      leakBaseMultiplierPL->get(PT_),
--- World/AsteroidTeamGazingWorld/AsteroidTeamGazingWorld.h
+++ World/AsteroidTeamGazingWorld/AsteroidTeamGazingWorld.h
@@ -1,9 +1,18 @@
 #pragma once
 
+#include <string>
+#include <memory>
+#include <vector>
+#include <map>
+
 #include "../AbstractSlideshowWorld.h"
 #include "../AsteroidGazingWorld/Utilities/AsteroidsDatasetParser.h"
 #include "../AsteroidGazingWorld/Sensors/AbsoluteFocusingSaccadingEyesSensors.h"
 #include "../AsteroidGazingWorld/Sensors/PeripheralAndRelativeSaccadingEyesSensors.h"
+
+#include "dag.h"
+
+typedef std::vector<std::vector<double>> StateTimeSeries;
 
 class AsteroidTeamGazingWorld : public AbstractSlideshowWorld {
 
@@ -20,23 +29,31 @@ private:
 	static std::shared_ptr<ParameterLink<bool>> integrateFitnessPL;
 	static std::shared_ptr<ParameterLink<int>> numTriggerBitsPL;
 	static std::shared_ptr<ParameterLink<int>> numRandomInitialConditionsPL;
-	static std::shared_ptr<ParameterLink<int>> compressToBitsPL;
 	static std::shared_ptr<ParameterLink<int>> fastRepellingPLInfoNumNeighborsPL;
 	static std::shared_ptr<ParameterLink<int>> mihPatternChunkSizeBitsPL;
 	static std::shared_ptr<ParameterLink<double>> leakBaseMultiplierPL;
 	static std::shared_ptr<ParameterLink<double>> leakDecayRadiusPL;
+	static std::shared_ptr<ParameterLink<std::string>> pathToBrainTreeJSONPL;
 
 	std::shared_ptr<AsteroidsDatasetParser> datasetParser;
-
-	bool resetAgentBetweenStates() override { return true; };
-	int brainUpdatesPerWorldState() override { return brainUpdatesPerAsteroid; };
-
 	std::shared_ptr<std::string> currentAsteroidName;
-
 	static int initialConditionsInitialized;
 	static std::map<std::string,std::vector<Range2d>> commonRelativeSensorsInitialConditions;
 
-// 	std::vector<std::shared_ptr<AbstractBrain>> brains;
+	unsigned numBrains;
+	DAGraph<int> brainsDiagram;
+	std::vector<std::shared_ptr<AbstractBrain>> brains; // maybe replace with map-to-structures at some point
+	std::vector<std::string> brainsIDs;
+	std::vector<unsigned> brainsEvolvable;
+	std::vector<unsigned> brainsNumOutputs;
+	std::vector<unsigned> brainsNumHidden;
+	std::vector<unsigned> brainsInputCached; // format: 0 = do not cache, 1 = input to be cached, 2 = input cached
+	std::vector<StateTimeSeries> brainsInputCache; // preference for fast evolvable end point brains over the fast combined constant-evolvable end point brains
+	std::vector<unsigned> exposedOutputs;
+	unsigned numBrainsOutputs;
+
+	bool resetAgentBetweenStates() override { return true; };
+	int brainUpdatesPerWorldState() override { return brainUpdatesPerAsteroid; };
 
 public:
 	AsteroidTeamGazingWorld(std::shared_ptr<ParametersTable> PT_);
