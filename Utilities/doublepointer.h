85470916bc5acb35da0641067fb3ff0f2abec796
Anton Bernatskiy (kohama) <abernats@uvm.edu>
2020-04-18 05:09:51 -0400

    Implementation of the funny pointers
    Since MABE only allows passing std::vector<double>'s between
    Worlds and Brains, and I happen to need to pass 4D tensors, I'm
    going to encode the pointers into doubles and back. This commit
    contains the implementation of the encoding/decoding mechanism.
    
    	new file:   Utilities/doublepointer.h

---
+++ Utilities/doublepointer.h
@@ -0,0 +1,33 @@
+#include <cassert>
+#include <vector>
+
+namespace doublePointer {
+
+	void testEnvironment() {
+		assert(sizeof(void*)==sizeof(uint64_t));
+	};
+
+	void toDoubles(void* ptr, std::vector<double>::iterator outputIterator) {
+		// will write the pointer data to *outputIterator and *(outputIterator+1)
+		uint64_t iptr = reinterpret_cast<uint64_t>(ptr);
+		*outputIterator = static_cast<double>(iptr & 0x00000000ffffffff);
+		*(outputIterator+1) = static_cast<double>((iptr & 0xffffffff00000000) >> 32);
+	};
+
+	void* fromDoubles(std::vector<double>::iterator inputIterator) {
+		// will read the pointer data from *outputIterator and *(outputIterator+1)
+		uint64_t iptr = ( static_cast<uint64_t>(*(inputIterator+1)) << 32 ) | ( static_cast<uint64_t>(*inputIterator) );
+		return reinterpret_cast<void*>(iptr);
+	};
+
+	template<typename StoredDataType>
+	void toDoubles(StoredDataType* ptr, std::vector<double>::iterator outputIterator) {
+		toDoubles(reinterpret_cast<void*>(ptr), outputIterator);
+	};
+
+	template<typename StoredDataType>
+	StoredDataType* fromDoubles(std::vector<double>::iterator inputIterator) {
+		return reinterpret_cast<StoredDataType*>(fromDoubles(inputIterator));
+	};
+
+} // namespace doublePointer
