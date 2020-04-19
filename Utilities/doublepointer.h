3847be11c105d92e678bd058069ad88c35eb0adf
Anton Bernatskiy (kohama) <abernats@uvm.edu>
2020-04-18 22:52:26 -0400

    Muxa's comments implemented
    	modified:   Utilities/doublepointer.h

--- Utilities/doublepointer.h
+++ Utilities/doublepointer.h
@@ -1,17 +1,14 @@
-#include <cassert>
 #include <vector>
 
-namespace doublePointer {
+static_assert(sizeof(void*)==sizeof(uint64_t));
 
-	void testEnvironment() {
-		assert(sizeof(void*)==sizeof(uint64_t));
-	};
+namespace doublePointer {
 
 	void toDoubles(void* ptr, std::vector<double>::iterator outputIterator) {
 		// will write the pointer data to *outputIterator and *(outputIterator+1)
 		uint64_t iptr = reinterpret_cast<uint64_t>(ptr);
 		*outputIterator = static_cast<double>(iptr & 0x00000000ffffffff);
-		*(outputIterator+1) = static_cast<double>((iptr & 0xffffffff00000000) >> 32);
+		*(outputIterator+1) = static_cast<double>(iptr >> 32);
 	};
 
 	void* fromDoubles(std::vector<double>::iterator inputIterator) {
